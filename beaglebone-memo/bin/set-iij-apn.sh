750e08962897d0a10796a2c59bdc47ae4da4b5cb
Mototsugu Ohgami <gamix255@gmail.com>
2015-05-14 23:39:49 +0000

    add comment and fix file create date

--- beaglebone-memo/bin/set-iij-apn.sh
+++ beaglebone-memo/bin/set-iij-apn.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+#require adb command from android-tools-adb
+#testing by debian 7 and beaglebone
+
 adb shell settings put global tether_dun_apn IIJmio,iijmio.jp,,,mio@iij,iij,,,,,440,10,3,*
