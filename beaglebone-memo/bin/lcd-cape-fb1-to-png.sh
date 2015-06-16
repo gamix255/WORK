636fa644c12c98b7139687ec1a8374bf917775ff
Mototsugu Ohgami <gamix255@gmail.com>
2015-06-16 12:56:51 +0000

    add comment and fix file create date

--- beaglebone-memo/bin/lcd-cape-fb1-to-png.sh
+++ beaglebone-memo/bin/lcd-cape-fb1-to-png.sh
@@ -1,2 +1,15 @@
+#!/bin/sh
+
+# fb device to png script. require ffmpeg command.
+# pixel format check by fbset command. provide by fbset package.
+
+# #fbset
+# mode "480x272"
+#     geometry 480 272 480 272 16
+#     timings 0 0 0 0 0 0 0
+#     accel true
+#     rgba 5/11,6/5,5/0,0/0
+# endmode
+
 cat /dev/fb0 > rawvideo2
 ffmpeg -vframes 1 -vcodec rawvideo -f rawvideo -pix_fmt rgb565le -s 480x272 -i rawvideo2 -f image2 -vcodec png image1.png
