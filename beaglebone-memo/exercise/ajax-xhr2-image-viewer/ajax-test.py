d8752c7594ea6526d28bac2036506cf6f23d266e
Mototsugu Ohgami <gamix255@gmail.com>
2015-07-01 23:52:08 +0900

    ajax xhr2 test to begin

---
+++ beaglebone-memo/exercise/ajax-xhr2-image-viewer/ajax-test.py
@@ -0,0 +1,56 @@
+from bottle import route, run
+
+@route('/screen')
+
+def screen():
+  return """
+
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html;">
+<title>xhr2 image loading test</title>
+<script type="text/javascript">
+ 
+<!--
+setInterval("loadIMG()",5000);
+window.URL = window.URL || window.webkitURL;
+ 
+function loadIMG(){
+  var xhr;
+  xhr = new XMLHttpRequest();
+  console.log("loadIMG entry");
+  
+  xhr.open("GET","/static/img.png",true);
+  xhr.responseType = 'blob';
+  xhr.onload = function () {
+    var blob, img;
+    console.log("xhr onload");
+    if(xhr.status === 200 ){
+    console.log("xhr status === 200");
+    blob = xhr.response;
+    img = document.createElement('img');
+
+    img.onload = function() {
+      window.URL.revokeObjectURL(img.src);
+      };
+      img.src = window.URL.createObjectURL(blob);
+//      document.body.appendChild(img);
+
+//      var oldE = document.body.lastChild;
+//      document.body.replaceChild(img, oldE);
+      document.screenshot.src=img.src;
+    }
+  };
+  xhr.send();
+};
+//-->
+</script>
+</head>
+<body>
+<p class="image1">
+<img src="/static/img_org.png" alt="screenshot" name="screenshot"><br>
+</p>
+</body>
+</html>
+"""
+run(host='localhost', port=8080)
