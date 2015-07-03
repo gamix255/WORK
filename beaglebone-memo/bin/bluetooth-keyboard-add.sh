29f8a41702c942622ba5fd529f3ed8622fd21eff
root <root@beaglebone.(none)>
2015-07-03 13:01:03 +0900

    initial bluez and bluetooth keyboard use script add

---
+++ beaglebone-memo/bin/bluetooth-keyboard-add.sh
@@ -0,0 +1,60 @@
+
+
+# beaglebone and bluetooth dongle use keyboard
+# dongle: planex BT-Micro3E2X
+
+# checkpoint1: lsusb
+# require: bluez package from debian
+
+# hcitool scan
+#Scanning ...
+#        AA:BB:CC:DD:EE:FF       Nintendo Wireless Keyboard
+
+# memo:bluetooth package install message  
+
+# The following extra packages will be installed:
+#  bc bluez bluez-alsa bluez-cups bluez-gstreamer cups cups-client cups-common
+#  cups-filters cups-ppdc dc fonts-droid fonts-freefont-ttf
+#  foomatic-db-compressed-ppds foomatic-db-engine foomatic-filters ghostscript
+#  ghostscript-cups gsfonts hpijs hplip hplip-data libart-2.0-2 libcups2
+#  libcupscgi1 libcupsdriver1 libcupsfilters1 libcupsimage2 libcupsmime1
+#  libcupsppdc1 libescpr1 libgs9 libgs9-common libgutenprint2 libhpmud0
+#  libijs-0.35 libjbig2dec0 liblcms1 libpaper-utils libpaper1 libpoppler19
+#  libsane-hpaio libslp1 mscompress poppler-data poppler-utils
+#  printer-driver-all printer-driver-c2050 printer-driver-c2esp
+#  printer-driver-cjet printer-driver-escpr printer-driver-foo2zjs
+#  printer-driver-gutenprint printer-driver-hpcups printer-driver-hpijs
+#  printer-driver-m2300w printer-driver-min12xxw printer-driver-pnm2ppa
+#  printer-driver-postscript-hp printer-driver-ptouch printer-driver-pxljr
+#  printer-driver-sag-gdi printer-driver-splix python-imaging python-pexpect
+#  python-renderpm python-reportlab python-reportlab-accel samba-common
+#  samba-common-bin smbclient ttf-freefont unzip
+#Suggested packages:
+#  cups-bsd cups-pdf xpp hplip-cups openprinting-ppds cjet
+#  foomatic-db-gutenprint ghostscript-x hplip-gui hplip-doc
+#  system-config-printer gutenprint-locales liblcms-utils slpd openslp-doc
+#  fonts-japanese-mincho fonts-ipafont-mincho fonts-japanese-gothic
+#  fonts-ipafont-gothic fonts-arphic-ukai fonts-arphic-uming fonts-unfonts-core
+#  psutils hannah-foo2zjs tk8.4 tix gutenprint-doc hpijs-ppds magicfilter
+#  apsfilter python-imaging-doc python-imaging-dbg python-renderpm-dbg
+#  pdf-viewer python-egenix-mxtexttools python-reportlab-doc cifs-utils zip
+#Recommended packages:
+#  foomatic-db
+#The following NEW packages will be installed:
+#  bc bluetooth bluez bluez-alsa bluez-cups bluez-gstreamer cups cups-client
+#  cups-common cups-filters cups-ppdc dc fonts-droid fonts-freefont-ttf
+#  foomatic-db-compressed-ppds foomatic-db-engine foomatic-filters ghostscript
+#  ghostscript-cups gsfonts hpijs hplip hplip-data libart-2.0-2 libcupscgi1
+#  libcupsdriver1 libcupsfilters1 libcupsimage2 libcupsmime1 libcupsppdc1
+#  libescpr1 libgs9 libgs9-common libgutenprint2 libhpmud0 libijs-0.35
+#  libjbig2dec0 liblcms1 libpaper-utils libpaper1 libpoppler19 libsane-hpaio
+#  libslp1 mscompress poppler-data poppler-utils printer-driver-all
+#  printer-driver-c2050 printer-driver-c2esp printer-driver-cjet
+#  printer-driver-escpr printer-driver-foo2zjs printer-driver-gutenprint
+#  printer-driver-hpcups printer-driver-hpijs printer-driver-m2300w
+#  printer-driver-min12xxw printer-driver-pnm2ppa printer-driver-postscript-hp
+#  printer-driver-ptouch printer-driver-pxljr printer-driver-sag-gdi
+#  printer-driver-splix python-imaging python-pexpect python-renderpm
+#  python-reportlab python-reportlab-accel samba-common samba-common-bin
+#  smbclient ttf-freefont unzip
+#
