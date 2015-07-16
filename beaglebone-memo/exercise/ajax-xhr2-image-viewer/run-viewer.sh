e9abd83509abe7a0a2449798383ffcc172ac0ae0
Mototsugu Ohgami <gamix255@gmail.com>
2015-07-16 12:59:39 +0900

    sleep interval change to 0.1 sec

--- beaglebone-memo/exercise/ajax-xhr2-image-viewer/run-viewer.sh
+++ beaglebone-memo/exercise/ajax-xhr2-image-viewer/run-viewer.sh
@@ -2,7 +2,10 @@ sh stop-viewer.sh
 cp -pR ../ajax-xhr2-image-viewer/ /ram/
 cd /ram/ajax-xhr2-image-viewer/
 python ./ajax-test.py &
-for i in `seq 0 3600`; do sleep 1; sh lcd-cape-fb1-to-png.sh ; done
+while :
+ do sleep 0.1
+ sh lcd-cape-fb1-to-png.sh > /dev/null 2>&1 
+done
 sh stop-viewer.sh
 
 
