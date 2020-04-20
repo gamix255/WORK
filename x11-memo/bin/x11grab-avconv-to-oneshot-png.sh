bc7ba14727e25b38282fcb3c8599c847b1f10c25
Mototsugu Ohgami <gamix255@gmail.com>
2020-04-20 20:18:07 +0900

    Update x11grab-avconv-to-oneshot-png.sh

--- x11-memo/bin/x11grab-avconv-to-oneshot-png.sh
+++ x11-memo/bin/x11grab-avconv-to-oneshot-png.sh
@@ -1,2 +1,1 @@
- avconv -s "$(xwininfo -root | grep -Po 'geometry[^+]*' | sed 's/^\S* //')" -f x11grab -i :0.0 -r 1 -f image2 imagename.png  
-
+avconv -s "$(xwininfo -root | grep -Po 'geometry[^+]*' | sed 's/^\S* //')" -f x11grab -i :0.0 -r 1 -f image2 -frames:v 1  imagename.png
